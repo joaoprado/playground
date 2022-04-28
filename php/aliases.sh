c22f7e38265c657f7d451d254dae6513e1e55312
Joao Prado <contato@joaoprado.com.br>
2022-04-28 15:44:26 -0300

    Atualizar imagens php

--- php/aliases.sh
+++ php/aliases.sh
@@ -6,7 +6,6 @@ alias serve='art serve'
 alias tinker='art tinker'
 alias pest='vendor/bin/pest'
 alias rollback='art migrate:rollback'
-alias utest='vendor/bin/pest --stop-on-error --stop-on-failure tests/Unit'
 alias cc='art cache:clear && art config:clear'
 alias migrate='art migrate'
 alias phpunit='vendor/bin/phpunit'
@@ -16,4 +15,43 @@ alias gclear="composer clear-generated-files"
 alias gclient="composer create-client-files"
 alias gserver="composer create-server-interface"
 
-export PATH=$PATH:/usr/grpc/grpc/cmake/build:/usr/grpc/grpc/cmake/build/third_party/protobuf
+alias artisan='php artisan'
+alias bob='php artisan bob::build'
+
+# Development
+alias pas='php artisan serve'
+
+# Database
+alias pam='php artisan migrate'
+alias pamf='php artisan migrate:fresh'
+alias pamfs='php artisan migrate:fresh --seed'
+alias pamr='php artisan migrate:rollback'
+alias pads='php artisan db:seed'
+
+# Makers
+alias pamm='php artisan make:model'
+alias pamc='php artisan make:controller'
+alias pams='php artisan make:seeder'
+alias pamt='php artisan make:test'
+alias pamfa='php artisan make:factory'
+alias pamp='php artisan make:policy'
+alias pame='php artisan make:event'
+alias pamj='php artisan make:job'
+alias paml='php artisan make:listener'
+alias pamn='php artisan make:notification'
+alias pampp='php artisan make:provider'
+
+
+# Clears
+alias pacac='php artisan cache:clear'
+alias pacoc='php artisan config:clear'
+alias pavic='php artisan view:clear'
+alias paroc='php artisan route:clear'
+
+# queues
+alias paqf='php artisan queue:failed'
+alias paqft='php artisan queue:failed-table'
+alias paql='php artisan queue:listen'
+alias paqr='php artisan queue:retry'
+alias paqt='php artisan queue:table'
+alias paqw='php artisan queue:work'
